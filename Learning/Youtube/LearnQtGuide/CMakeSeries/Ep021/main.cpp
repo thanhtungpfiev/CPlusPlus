a869fab75f18cb4e02f82d1221ca0a3654380dda
Dao Thanh Tung (MS/EDA33-XC) <tung.daothanh@vn.bosch.com>
2024-11-13 11:28:55 +0700

    CMakeSeries v10

---
+++ Learning/Youtube/LearnQtGuide/CMakeSeries/Ep021/main.cpp
@@ -0,0 +1,14 @@
+#include <fmt/format.h>
+#include <iostream>
+#include "person.h"
+
+int main() {
+  Person p("John", 42);
+  std::cout << fmt::format("{} is {} years old.", p.getName(), p.getAge())
+            << std::endl;
+  Person p2("Jane", 43);
+  std::cout << fmt::format("{} is {} years old.", p2.getName(), p2.getAge())
+            << std::endl;
+  fmt::println("Hello, World!");
+  return 0;
+}
