f9b9f6ab672af444dace9e34f1dcf8998ab2b123
Dao Thanh Tung (MS/EDA33-XC) <tung.daothanh@vn.bosch.com>
2024-11-13 09:49:04 +0700

    CMakeSeries v9

---
+++ Learning/Youtube/LearnQtGuide/CMakeSeries/Ep020/person.h
@@ -0,0 +1,28 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+
+class Person {
+ private:
+  std::string name;
+  int age;
+
+ public:
+  // Constructor
+  Person(const std::string& name, int age);
+
+  // Getter for name
+  std::string getName() const;
+
+  // Setter for name
+  void setName(const std::string& newName);
+
+  // Getter for age
+  int getAge() const;
+
+  // Setter for age
+  void setAge(int newAge);
+};
+
+#endif /* PERSON_H */
