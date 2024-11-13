8eefb8b1e17ddfdf53ae35045615c41fe3a8b64c
Dao Thanh Tung (MS/EDA33-XC) <tung.daothanh@vn.bosch.com>
2024-11-13 13:28:49 +0700

    CMakeSeries v11

---
+++ Learning/Youtube/LearnQtGuide/CMakeSeries/Ep022/src/calculator.h
@@ -0,0 +1,14 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator
+{
+public:
+    int add(int, int);
+    int sub(int, int);
+    int mul(int, int);
+    int div(int, int);
+    int mod(int, int);
+};
+
+#endif /* CALCULATOR_H */
