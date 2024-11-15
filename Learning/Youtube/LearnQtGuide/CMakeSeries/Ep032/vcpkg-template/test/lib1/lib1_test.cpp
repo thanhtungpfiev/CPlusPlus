f59567304f92be7363e39788a45571b2b1461994
Dao Thanh Tung (MS/EDA33-XC) <tung.daothanh@vn.bosch.com>
2024-11-15 14:40:45 +0700

    CMakeSeries v21

---
+++ Learning/Youtube/LearnQtGuide/CMakeSeries/Ep032/vcpkg-template/test/lib1/lib1_test.cpp
@@ -0,0 +1,42 @@
+#include "lib1.h"
+
+#include <sstream>
+
+#include "gtest/gtest.h"
+
+namespace {
+
+class Lib1Test : public ::testing::Test {
+ protected:
+  lib1::MyClass1 obj;
+  std::stringstream buffer;
+  std::streambuf *sbuf;
+  void SetUp() {
+    sbuf = std::cout.rdbuf();
+    std::cout.rdbuf(buffer.rdbuf());
+  }
+  void TearDown() { std::cout.rdbuf(sbuf); }
+};
+
+TEST_F(Lib1Test, Function1Test) {
+  obj.function1();
+  std::string expected = "lib1::MyClass1::function1\n";
+  EXPECT_EQ(buffer.str(), expected);
+  buffer.str(std::string());
+}
+
+TEST_F(Lib1Test, Function2Test) {
+  obj.function2();
+  std::string expected = "lib1::MyClass1::function2\n";
+  EXPECT_EQ(buffer.str(), expected);
+  buffer.str(std::string());
+}
+
+TEST_F(Lib1Test, Function3Test) {
+  obj.function3();
+  std::string expected = "lib1::MyClass1::function3\n";
+  EXPECT_EQ(buffer.str(), expected);
+  buffer.str(std::string());
+}
+
+}  // namespace
