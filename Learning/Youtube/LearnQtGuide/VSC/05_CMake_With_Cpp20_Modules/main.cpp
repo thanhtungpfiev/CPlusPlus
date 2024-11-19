531171a40fde5760d893b4c8266bf0892b254f2b
Dao Thanh Tung (MS/EDA33-XC) <tung.daothanh@vn.bosch.com>
2024-11-19 11:05:17 +0700

    VSC v4

---
+++ Learning/Youtube/LearnQtGuide/VSC/05_CMake_With_Cpp20_Modules/main.cpp
@@ -0,0 +1,30 @@
+/*
+    . Using cmake presets with c++20 modules
+    . On windows with visual studio, be sure to explicitly use the visual studio generator.
+    . Reference: 
+        . Install ninja 1.11 on ubuntu: https://lindevs.com/install-ninja-build-system-on-ubuntu
+    . Pros:
+        . Easy to set up a debugger.
+        . Relatively easy to bring in third party dependencies through FetchContent. 
+       . Easy to share project specific settings among developers and IDEs.
+
+    . Cons:
+        . Requires CMake to be installed on the system.
+        . CMake 3.28 or higher is required.
+        . Currently works out of the box for MSVC and Clang16+.
+*/
+
+#include <fmt/format.h>
+import calculator;
+
+int main() {
+  int value{5};
+  int count{10};
+  Calculator calc;
+  auto result = calc.add(value, count);
+
+  fmt::println("The value is: {}", value);
+  fmt::println("The sky is blue my friend!");
+  fmt::println("The result is: {}", result);
+  return 0;
+}
